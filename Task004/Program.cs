be4538ff285c94ad42fb91eba18050cfc7d5b3e6
Alezar13 <AZgames13@gmail.com>
2023-06-30 11:16:58 +0300

    Add task004

---
+++ Task004/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("ВВедите число =>");
+String Inputed = Console.ReadLine();
+int A = Convert.ToInt32(Inputed);
+for (int count = 2; count <= A; count++){
+    if (count % 2 == 0)
+    System.Console.WriteLine(count);
+}
