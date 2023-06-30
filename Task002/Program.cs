3ea30e83787a31365b99e2e5a55019e5ccd1f109
Alezar13 <AZgames13@gmail.com>
2023-06-30 09:13:24 +0300

    Add task002

---
+++ Task002/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("ВВедите число 1=>");
+String Inputed1 = Console.ReadLine();
+int A = Convert.ToInt32(Inputed1);
+Console.Write("ВВедите число 2=>");
+String Inputed2 = Console.ReadLine();
+int B = Convert.ToInt32(Inputed2);
+Console.Write("ВВедите число 3=>");
+String Inputed3 = Console.ReadLine();
+int C = Convert.ToInt32(Inputed3);
+int max = A;
+if (A > B) max = A;
+if (B > A) max = B;
+if (C > B) max = C;
+Console.Write ("max =");
+Console.WriteLine (max);
