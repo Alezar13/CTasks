899d446fb58df56d8472d7b54bb2f4ce224e030b
Alezar13 <AZgames13@gmail.com>
2023-06-30 08:43:36 +0300

    Добавил ввод числа

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,6 +1,10 @@
-﻿Console.Clear() ;
-int A = 15;
-int B = 9;
+﻿Console.Clear();
+Console.Write("ВВедите число=>");
+String Inputed1 = Console.ReadLine();
+int A = Convert.ToInt32(Inputed1);
+Console.Write("ВВедите число=>");
+String Inputed2 = Console.ReadLine();
+int B = Convert.ToInt32(Inputed2);
 int max = A;
 if (A > B) max = A;
 if (B > A) max = B;
