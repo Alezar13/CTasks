1576a73d76b27a5efc9ab254dd55604670d0e44d
Alezar13 <AZgames13@gmail.com>
2023-06-30 09:54:46 +0300

    Add task003

---
+++ Task003/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("ВВедите число =>");
+String Inputed = Console.ReadLine();
+int A = Convert.ToInt32(Inputed);
+if (A % 2 == 0)
+{
+    Console.WriteLine ("Четное число");
+}
+else
+{
+    System.Console.WriteLine("Число нечетное");
+}
