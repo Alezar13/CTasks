f5025d2a158f9cb6be5434bea0c5dce848761ab0
Alezar13 <AZgames13@gmail.com>
2023-07-01 10:50:21 +0300

    Add Task005

---
+++ Task005/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+int Prompt (string message)
+{
+Console.Write("ВВедите число =>");
+String Inputed = Console.ReadLine();
+int res = Convert.ToInt32(Inputed);
+return res;
+}
+
+int num = Prompt ("Введите трехзначное число =>");
+if (num < 100 || num >= 1000)
+{
+    System.Console.WriteLine("Не трехзначное число");
+    return;
+}
+System.Console.WriteLine($"Введеное число {num}");
+int secNum = num / 10 % 10;
+System.Console.WriteLine($"Вторая цифра числа {num} это {secNum}");
