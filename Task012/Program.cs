54e6c52c96d549688f90a645d5c896dcaa9d71ab
Alezar13 <AZgames13@gmail.com>
2023-07-07 23:59:23 +0300

    Add Task012

---
+++ Task012/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число и 
+// выдаёт сумму цифр в числе.
+Console.Clear();
+int Prompt (string message)
+{
+Console.Write(message);
+String Input = Console.ReadLine();
+int res = int.Parse(Input);
+return res;
+}
+int Sum (int num)
+{
+    int res = 0;
+    while (num > 0)
+    {
+        res += num % 10;
+        num = num / 10;
+    }
+    return res;
+}
+int num = Prompt ("Введите положительное число не более 2147483647=>");
+System.Console.WriteLine($"Сумма всех цифр в числе {num} = {Sum(num)}");
