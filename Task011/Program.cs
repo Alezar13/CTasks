9727846d0513f4b4f87571664a4ef2aeb0c8e2da
Alezar13 <AZgames13@gmail.com>
2023-07-07 23:14:11 +0300

    Add Task011

---
+++ Task011/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B. Сделать через функции.
+Console.Clear();
+int Prompt (string message)
+{
+Console.Write(message);
+String Input = Console.ReadLine();
+int res = int.Parse(Input);
+return res;
+}
+int Power(int powerBase, int exponent)
+{
+    int Power = 1;
+    for (int i = 0; i < exponent; i++)
+    {
+        Power *= powerBase;
+    }
+    return Power;
+}
+bool ValidateExponent (int exponent)
+{
+    if (exponent < 0)
+    {
+        System.Console.WriteLine("Показатель должен быть больше нуля");
+        return false;
+    }
+    return true;
+}
+int powerBase = Prompt ("Введите основание: ");
+int exponent = Prompt ("Введите показатель: ");
+if (ValidateExponent (exponent))
+{
+    System.Console.WriteLine($"Число {powerBase} в степени {exponent} равно {Power (powerBase, exponent)}");
+}
