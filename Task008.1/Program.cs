be9cb646b654da09788a1a8d8f187b4d3ce6dc30
Alezar13 <AZgames13@gmail.com>
2023-07-08 11:45:52 +0300

    Add Task008.1

---
+++ Task008.1/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите программу, которая принимает на вход число "N" и проверяет, является ли оно палиндромом.
+Console.Clear();
+int Prompt (string message)
+{
+Console.Write("Введите число =>");
+String Inputed = Console.ReadLine();
+int res = Convert.ToInt32(Inputed);
+return res;
+}
+int num = Prompt("Введите число =>");
+int left = num;
+int rev = 0;
+while(Convert.ToBoolean(left)) 
+{
+   int r = left % 10;   
+   rev = rev * 10 + r;
+   left = left / 10;  
+}
+if(num==rev)
+{
+    System.Console.WriteLine($"Число {num} палиндром");
+}
+else
+{
+    System.Console.WriteLine($"Число {num} не палиндром");
+} 
+
